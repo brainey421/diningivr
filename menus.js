35aa0a8d55113ae2517d5f352751b0d32bfa2a74
brainey421 <raineyb@purdue.edu>
2014-10-18 11:45:43 -0400

    I did something last night.

--- menus.js
+++ menus.js
@@ -27,8 +27,13 @@ exports.getMenu = function(diningCourtId) {
 	}
 }
 
+//var xmlHttp = new XMLHttpRequest();
+//xmlHttp.open("GET", "http://api.hfs.purdue.edu/menus/v1/locations/Earhart/10-18-2014", false);
+//xmlHttp.send(null);
+//console.log(xmlHttp.responseText);
+
 //http.get("http://api.hfs.purdue.edu/menus/v1/locations/Earhart/10-18-2014", function(data) {
-//  console.log(data);
-//}).on('error', function(e) {
-//  console.log("Got error: " + e.message);
-//});
+  //console.log(data);
+  //}).on('error', function(e) {
+  //console.log("Got error: " + e.message);
+  //});
