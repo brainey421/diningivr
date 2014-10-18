06e6d7d8eb3775cb054dc1a22835910abf743f9e
Ben Wencke <benwencke@gmail.com>
2014-10-18 14:25:18 -0400

    Dont remember what I did

--- menus.js
+++ menus.js
@@ -35,58 +35,44 @@ exports.getMenu = function(diningCourtId) {
 	
 	console.log("Loading menu: " + "http://api.hfs.purdue.edu/menus/v1/locations/" + exports.diningCourts[diningCourtId] + "/" + month + "-" + day + "-" + year);
 	
-	var http = require("http");
-	var menu = "";
-	http.get("http://api.hfs.purdue.edu/menus/v1/locations/" + exports.diningCourts[diningCourtId] + "/" + month + "-" + day + "-" + year, function(response) {
-		response.on("data", function(chunk) {
-		    menu += chunk;
-		});
-		response.on("end", function() {
-			menu = JSON.parse(menu);
+	var httpsync = require("httpsync");
+	var menu = httpsync.get("http://api.hfs.purdue.edu/menus/v1/locations/" + exports.diningCourts[diningCourtId] + "/" + month + "-" + day + "-" + year);
+	menu = JSON.parse(menu);
 			
-			var meal = "";
-			if (hour < 9 || hour == 9 && minute < 30)
+	var meal = "";
+	if (hour < 9 || hour == 9 && minute < 30)
+	{
+		meal = "Breakfast";
+	}
+	else if (hour < 14)
+	{
+		meal = "Lunch";
+	}
+	else
+	{
+		meal = "Dinner";
+	}
+	
+	ret = "";
+	for (var i = 0; i < menu[meal].length; i++)
+	{
+		items = menu[meal][i]["Items"];
+		for (var j = 0; j < items.length; j++)
+		{
+			if (i == menu[meal].length - 1 && j == items.length - 1)
 			{
-				meal = "Breakfast";
-			}
-			else if (hour < 14)
-			{
-				meal = "Lunch";
+				ret += "and " + items[j]["Name"] + " for " + meal + ".";
 			}
 			else
 			{
-				meal = "Dinner";
+				ret += items[j]["Name"] + ", ";
 			}
-			
-			ret = "";
-			for (var i = 0; i < menu[meal].length; i++)
-			{
-				items = menu[meal][i]["Items"];
-				for (var j = 0; j < items.length; j++)
-				{
-					if (i == menu[meal].length - 1 && j == items.length - 1)
-					{
-						ret += "and " + items[j]["Name"] + " for " + meal + ".";
-					}
-					else
-					{
-						ret += items[j]["Name"] + ", ";
-					}
-				}
-			}
-			if (ret == "")
-			{
-				ret = "nothing for " + meal + "."
-			}
-			
-			//console.log(ret);
-			str = ret;
-			done = true;
-			return ret;
-		});
-	});
-	while(!done) {
-
+		}
 	}
-	return str;
+	if (ret == "")
+	{
+		ret = "nothing for " + meal + "."
+	}
+			
+	return ret;
 }
