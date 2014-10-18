8a75a04c89ded108b3057a80359184f773b9eccc
brainey421 <raineyb@purdue.edu>
2014-10-18 04:49:08 -0400

    Get meal time.

--- menus.js
+++ menus.js
@@ -1,8 +1,34 @@
+var http = require("http");
+
 var diningCourts = [
+	"Earhart",
+	"Ford",
+	"Hillenbrand",
 	"Wiley",
 	"Windsor"
 ];
 
 exports.getMenu = function(diningCourtId) {
-	return "pizza";
+	var date = new Date();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	
+	if (hour < 9 || hour == 9 && minute < 30)
+	{
+		return "breakfast";
+	}
+	else if (hour < 14)
+	{
+		return "lunch";
+	}
+	else
+	{
+		return "dinner";
+	}
 }
+
+//http.get("http://api.hfs.purdue.edu/menus/v1/locations/Earhart/10-18-2014", function(data) {
+//  console.log(data);
+//}).on('error', function(e) {
+//  console.log("Got error: " + e.message);
+//});
